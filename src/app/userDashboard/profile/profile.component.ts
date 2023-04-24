0977dcb8d070ae90cf234e3537c0b52740e31698
sirigidivamsikrishna <vamsikrishna.sirigidi@imaginnovate.com>
2023-04-24 11:15:45 +0530

    change

--- src/app/userDashboard/profile/profile.component.ts
+++ src/app/userDashboard/profile/profile.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
-
+  userData: any;
+  constructor() {}
+  ngOnInit(): void {
+    this.userData = {
+      profilePic:
+        'https://images.template.net/wp-content/uploads/2016/04/27043339/Nature-Wallpaper1.jpg',
+      coverPic: null,
+      firstName: 'dummy',
+      lastName: 'user',
+      userName: 'DUMMY001',
+      friend: true,
+      following: 20,
+      followers: 10,
+      posts: 2,
+      id: 2,
+      createdby: 'jan 15th 2001',
+      bio: 'he is dummy user',
+      link: 'ownbloglink.com',
+    };
+  }
 }
