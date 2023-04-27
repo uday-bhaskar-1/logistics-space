390f63a58c7d03454853d445453146644bbaf969
uday-bhaskar-1 <udaybhaskar.ambati@imaginnovate.com>
2023-04-27 10:32:36 +0530

    routing change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'',redirectTo:'auth',pathMatch:'full'},
   {path:'auth',
       loadChildren:()=>import ('./authentication/auth.module').then((m)=>m.AuthModule)
   },
