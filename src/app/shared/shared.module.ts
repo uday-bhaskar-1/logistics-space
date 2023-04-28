62b9695741f2c080a1af98cbe62ea6e187b4e650
sirigidivamsikrishna <vamsikrishna.sirigidi@imaginnovate.com>
2023-04-28 11:45:53 +0530

    postcomponentcreated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { PrimengModule } from './primeui/primeng.module';
 import { UsercardComponent } from './components/usercard/usercard.component';
+import { PostcardComponent } from './components/postcard/postcard.component';
 // import { TimePipe } from './pipes/time.pipe';
 
 @NgModule({
-  declarations: [UsercardComponent],
+  declarations: [UsercardComponent, PostcardComponent],
   imports: [CommonModule, PrimengModule],
   exports: [UsercardComponent, PrimengModule],
 })
