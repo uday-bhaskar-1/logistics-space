319cbdd6bd121508f4daa28dddf146a9670fd87e
sirigidivamsikrishna <vamsikrishna.sirigidi@imaginnovate.com>
2023-04-28 09:11:04 +0530

    postchanges

--- src/app/userDashboard/feedpost/feedpost.component.ts
+++ src/app/userDashboard/feedpost/feedpost.component.ts
@@ -7,7 +7,25 @@ import { Component } from '@angular/core';
 })
 export class FeedpostComponent {
   postContentText: any;
+  uploadImageUrls: any[] = [];
+  postImageUpload: any;
   constructor() {}
-  postPictureUpload(event) {}
+  postPictureUpload(event) {
+    const files: FileList = event.target.files;
+    const urls: string[] = [];
+    this.postImageUpload = event.target.files[0];
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        urls.push(reader.result as string);
+      };
+    }
+    this.uploadImageUrls = urls;
+  }
   postContentPost() {}
+  removeUploadImage(i) {
+    this.uploadImageUrls.splice(i, 1);
+  }
 }
