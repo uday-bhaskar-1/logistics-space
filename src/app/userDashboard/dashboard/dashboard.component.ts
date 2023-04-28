7f79b9d0413c048fde2a7ac0dc603dce8ad61098
uday-bhaskar-1 <udaybhaskar.ambati@imaginnovate.com>
2023-04-28 11:59:52 +0530

    changes

--- src/app/userDashboard/dashboard/dashboard.component.ts
+++ src/app/userDashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+
+  componentHeaderName;
+
+  constructor(private router:Router){  }
+
+  
+
+
+  fetchComponentHeaderName(){
+    let pageUrl = this.router.url;
+    this.componentHeaderName = pageUrl.split('/').splice(-1, 1)[0];
+    
+  }
+
 
 }
--- src/app/userDashboard/feedpost/feedpost.component.ts
+++ src/app/userDashboard/feedpost/feedpost.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-feedpost',
@@ -9,7 +10,52 @@ export class FeedpostComponent {
   postContentText: any;
   uploadImageUrls: any[] = [];
   postImageUpload: any;
-  constructor() {}
+  postsData: any[] = [];
+  userData: any;
+  commentData: any[] = [];
+  commentId: any;
+  commentGroup: FormGroup;
+  displayComments: boolean = false;
+  constructor(private fb: FormBuilder) {
+    this.commentGroup = this.fb.group({
+      comment: [''],
+    });
+  }
+  ngOnInit(): void {
+    this.postsData = [
+      {
+        firstName: 'dummy',
+        lastName: 'user',
+        userName: 'DUMMY001',
+        profilePic: null,
+        commentCount: 3,
+        imageUrl:
+          'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29tcGFueSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
+        like: true,
+        likeCount: 1,
+        likedTime: '2023-04-26T10:00:25.898857',
+        post: 'this is post',
+        postId: 6,
+        time: '2023-04-26T09:58:09.160662',
+      },
+      {
+        firstName: 'dummy',
+        lastName: 'user',
+        userName: 'DUMMY001',
+        profilePic: null,
+        commentCount: 3,
+        imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwiVmk8xECYpwcSBwvsrFawLKHEGGUT5JQ_w57FXE-VQ&usqp=CAU&ec=48665698',
+
+        like: true,
+        likeCount: 1,
+        likedTime: '2023-04-26T10:00:25.898857',
+        post: 'this is post 2',
+        postId: 6,
+        time: '2023-04-26T09:58:09.160662',
+      },
+    ];
+  }
   postPictureUpload(event) {
     const files: FileList = event.target.files;
     const urls: string[] = [];
@@ -28,4 +74,29 @@ export class FeedpostComponent {
   removeUploadImage(i) {
     this.uploadImageUrls.splice(i, 1);
   }
+  profilePictureUpload($event) {}
+  removeProfilePicture() {}
+  deletePost(postId) {}
+  likePost(postId) {}
+  getComments(postId) {
+    this.commentData = [
+      {
+        firstName: 'sanjay',
+        id: 5,
+        lastName: '111',
+        profilePic:
+          'https://storage.googleapis.com/download/storage/v1/b/phoenix-social/o/photos%2Fpexels-aleksandar-pasaric-2341830.jpg?generation=1682484241626079&alt=media',
+        userName: 'sanjay',
+      },
+      {
+        firstName: 'sanjay',
+        id: 5,
+        lastName: '111',
+        profilePic:
+          'https://storage.googleapis.com/download/storage/v1/b/phoenix-social/o/photos%2Fpexels-aleksandar-pasaric-2341830.jpg?generation=1682484241626079&alt=media',
+        userName: 'sanjay',
+      },
+    ];
+  }
+  userPostComment(postId) {}
 }
