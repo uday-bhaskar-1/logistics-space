3cb749d6f731fcce545e62b3b2cc4fd28820a23a
sirigidivamsikrishna <vamsikrishna.sirigidi@imaginnovate.com>
2023-04-28 12:04:28 +0530

    signout

--- src/app/userDashboard/settings/settings.component.ts
+++ src/app/userDashboard/settings/settings.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent {
-
+  constructor(private router: Router) {}
+  logout() {
+    this.router.navigate(['/auth']);
+  }
 }
